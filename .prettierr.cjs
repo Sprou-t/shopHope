72b1f2caef8db4a41e8dd5f26125073fb2a77102
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-09-17 02:14:50 +0800

    added prettier with eslint configuration

--- .prettierr.cjs
+++ .prettierr.cjs
@@ -1,8 +1,9 @@
 const config = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-  };
-  
-  export default config;
+  trailingComma: "es5",
+  tabWidth: 4, //dk why it is still 2
+  printWidth: 100, // length to which formatter would wrap on
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@
 import express from "express"; //"server": "json-server --watch db.json --port 3001"
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
-import productRoutes from './routes/product.route.js'
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config(); // looks for .env file in root directory by default
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // a middleware that allows app to accept data in req.body
 
-app.use('/products', productRoutes)
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
